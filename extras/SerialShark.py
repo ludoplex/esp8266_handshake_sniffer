aeb29520a290d1f2356e70cc4cb30a1cece454ec
Sourcery AI <>
2023-09-06 20:00:13 +0000

    'Refactored by Sourcery'

--- extras/SerialShark.py
+++ extras/SerialShark.py
@@ -9,10 +9,7 @@ import time
 
 try:
     serialportInput = input("[?] Select a serial port (default '/dev/ttyUSB0'): ")
-    if serialportInput == "":
-        serialport = "/dev/ttyUSB0"
-    else:
-        serialport = serialportInput
+    serialport = "/dev/ttyUSB0" if serialportInput == "" else serialportInput
 except KeyboardInterrupt:
     print("\n[+] Exiting...")
     exit()
@@ -23,7 +20,6 @@ try:
         boardRateInput = input("[?] Select a baudrate (default '115200'): ")
         if boardRateInput == "":
             boardRate = 115200
-            canBreak = True
         else:
             try:
                 boardRate = int(boardRateInput)
@@ -33,17 +29,14 @@ try:
             except Exception as e:
                 print("[!] Please enter a number!")
                 continue
-            canBreak = True
+        canBreak = True
 except KeyboardInterrupt:
     print("\n[+] Exiting...")
     exit()
 
 try:
     filenameInput = input("[?] Select a filename (default 'capture.pcap'): ")
-    if filenameInput == "":
-        filename = "capture.pcap"
-    else:
-        filename = filenameInput
+    filename = "capture.pcap" if filenameInput == "" else filenameInput
 except KeyboardInterrupt:
     print("\n[+] Exiting...")
     exit()
@@ -61,33 +54,26 @@ while not canBreak:
         time.sleep(2)
         continue
 
-print("[+] Serial connected. Name: " + ser.name)
+print(f"[+] Serial connected. Name: {ser.name}")
 counter = 0
-f = open(filename,'wb')
+with open(filename,'wb') as f:
+    check = 0
+    while check == 0:
+        line = ser.readline()
+        check=1
+    print("[+] Starting up wireshark...")
+    cmd = f"tail -f -c +0 {filename} | wireshark -k -i -"
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                           shell=True, preexec_fn=os.setsid)
 
-check = 0
-while check == 0:
-    line = ser.readline()
-    check=1
-#    if b"<<START>>" in line:
-#        check = 1
-#        print("[+] Stream started...")
-    #else: print '"'+line+'"'
+    try:
+        while True:
+            ch = ser.read()
+            f.write(ch)
+            f.flush()
+    except KeyboardInterrupt:
+        print("[+] Stopping...")
+        os.killpg(os.getpgid(p.pid), signal.SIGTERM)
 
-print("[+] Starting up wireshark...")
-cmd = "tail -f -c +0 " + filename + " | wireshark -k -i -"
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                       shell=True, preexec_fn=os.setsid)
-
-try:
-    while True:
-        ch = ser.read()
-        f.write(ch)
-        f.flush()
-except KeyboardInterrupt:
-    print("[+] Stopping...")
-    os.killpg(os.getpgid(p.pid), signal.SIGTERM)
-
-f.close()
 ser.close()
 print("[+] Done.")
